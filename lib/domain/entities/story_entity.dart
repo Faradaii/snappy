c1eb0c0ac22a8e34b6789cf0961fc71e1a86beef
Faradaii <kdkfrdy@gmail.com>
2025-01-13 22:33:34 +0800

    feat: domain layer architecture

---
+++ lib/domain/entities/story_entity.dart
@@ -0,0 +1,32 @@
+import 'package:equatable/equatable.dart';
+
+class Story extends Equatable {
+  String id;
+  String name;
+  String description;
+  String photoUrl;
+  DateTime createdAt;
+  double lat;
+  double lon;
+
+  Story({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.photoUrl,
+    required this.createdAt,
+    required this.lat,
+    required this.lon,
+  });
+
+  @override
+  List<Object?> get props => [
+    id,
+    name,
+    description,
+    photoUrl,
+    createdAt,
+    lat,
+    lon,
+  ];
+}
