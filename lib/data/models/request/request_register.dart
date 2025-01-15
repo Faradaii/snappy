82ae44843263bca2cc3ee1e80e71270dd321b6da
Faradaii <kdkfrdy@gmail.com>
2025-01-15 11:33:12 +0800

    feat: (datasources) add remote data source

--- lib/data/models/request/request_register.dart
+++ lib/data/models/request/request_register.dart
@@ -8,4 +8,7 @@ class RegisterRequest {
     required this.email,
     required this.password,
   });
+
+  Map<String, dynamic> toJson() =>
+      {'name': name, 'email': email, 'password': password};
 }
