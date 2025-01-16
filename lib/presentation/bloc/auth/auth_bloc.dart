e091091c12eaf8aeabf7fe57f5c33ceab878eac0
Faradaii <kdkfrdy@gmail.com>
2025-01-16 18:46:39 +0800

    feat: (bloc) add auth bloc

---
+++ lib/presentation/bloc/auth/auth_bloc.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:snappy/domain/usecases/auth_login_usecase.dart';
+import 'package:snappy/domain/usecases/auth_register_usecase.dart';
+
+import '../../../common/utils/data_state.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final LoginAuth authLoginUseCase;
+  final RegisterAuth authRegisterUseCase;
+
+  AuthBloc({required this.authLoginUseCase, required this.authRegisterUseCase})
+    : super(AuthInitialState()) {
+    on<AuthLoginEvent>((event, emit) async {
+      emit(const AuthLoadingState());
+      final result = await authLoginUseCase.execute(
+        event.email,
+        event.password,
+      );
+      result.fold(
+        (failure) => emit(AuthErrorState(failure.message)),
+        (success) => emit(AuthSuccessState(success.message)),
+      );
+    });
+
+    on<AuthRegisterEvent>((event, emit) async {
+      emit(const AuthLoadingState());
+      final result = await authRegisterUseCase.execute(
+        event.name,
+        event.email,
+        event.password,
+      );
+      result.fold(
+        (failure) => emit(AuthErrorState(failure.message)),
+        (success) => emit(AuthSuccessState(success.message)),
+      );
+    });
+  }
+}
---
+++ lib/presentation/bloc/auth/auth_state.dart
@@ -0,0 +1,26 @@
+part of 'auth_bloc.dart';
+
+sealed class AuthState {
+  final DataState dataState;
+  final String? message;
+
+  const AuthState({required this.dataState, this.message});
+}
+
+class AuthInitialState extends AuthState {
+  const AuthInitialState() : super(dataState: DataState.loading);
+}
+
+class AuthLoadingState extends AuthState {
+  const AuthLoadingState() : super(dataState: DataState.loading);
+}
+
+class AuthSuccessState extends AuthState {
+  const AuthSuccessState(String message)
+    : super(dataState: DataState.success, message: message);
+}
+
+class AuthErrorState extends AuthState {
+  const AuthErrorState(String message)
+    : super(dataState: DataState.error, message: message);
+}
